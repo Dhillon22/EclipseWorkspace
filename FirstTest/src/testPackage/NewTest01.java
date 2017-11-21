2373ac205625a31b044e1da18d34ca949697c68b
GS <gaganpreet_dhillon@infosys.com>
2017-11-21 15:25:20 +0530

    Added xml file for Test NG

--- FirstTest/src/testPackage/NewTest01.java
+++ FirstTest/src/testPackage/NewTest01.java
@@ -11,55 +11,61 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
 public class NewTest01 {
-	@Test
+	  @Test(priority = 0, groups = "GAGAN")
+
 	  public void f() {
-		  System.out.println("This is Test of 02");
+		  System.out.println("This is Test1 of 01 for Group GAGAN");
+	  }
+	  @Test(priority = 1, groups = "Amol")
+
+	  public void f2() {
+		  System.out.println("This is Test2 of 01 for Group Amol");
 	  }
 	  @BeforeMethod
 	  public void beforeMethod() {
-		  System.out.println("This is beforeMethod of 02");
+		  System.out.println("This is beforeMethod of 01");
 
 	  }
 
 	  @AfterMethod
 	  public void afterMethod() {
-		  System.out.println("This is afterMethod of 02");
+		  System.out.println("This is afterMethod of 01");
 
 	  }
 
 	  @BeforeClass
 	  public void beforeClass() {
-		  System.out.println("This is beforeClass of 02");
+		  System.out.println("This is beforeClass of 01");
 
 	  }
 
 	  @AfterClass
 	  public void afterClass() {
-		  System.out.println("This is afterClass of 02");
+		  System.out.println("This is afterClass of 01");
 
 	  }
 
 	  @BeforeTest
 	  public void beforeTest() {
-		  System.out.println("This is beforeTest of 02");
+		  System.out.println("This is beforeTest of 01");
 
 	  }
 
 	  @AfterTest
 	  public void afterTest() {
-		  System.out.println("This is afterTest of 02");
+		  System.out.println("This is afterTest of 01");
 
 	  }
 
 	  @BeforeSuite
 	  public void beforeSuite() {
-		  System.out.println("This is beforeSuite of 02");
+		  System.out.println("This is beforeSuite of 01");
 
 	  }
 
 	  @AfterSuite
 	  public void afterSuite() {
-		  System.out.println("This is afterSuite of 02");
+		  System.out.println("This is afterSuite of 01");
 
 	  }
 
--- FirstTest/src/testPackage/NewTest02.java
+++ FirstTest/src/testPackage/NewTest02.java
@@ -11,55 +11,59 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
 public class NewTest02 {
-  @Test
-  public void f() {
-	  System.out.println("This is Test of 01");
+  @Test(priority = 0, groups = "GAGAN")
+  public void f1() {
+	  System.out.println("This is Test1 of 02 for Group GAGAN");
+  }
+  @Test(priority = 1, groups = "Amol")
+  public void f2() {
+	  System.out.println("This is Test2 of 02 for Group Amol");
   }
   @BeforeMethod
   public void beforeMethod() {
-	  System.out.println("This is beforeMethod of 01");
+	  System.out.println("This is beforeMethod of 02");
 
   }
 
   @AfterMethod
   public void afterMethod() {
-	  System.out.println("This is afterMethod of 01");
+	  System.out.println("This is afterMethod of 02");
 
   }
 
   @BeforeClass
   public void beforeClass() {
-	  System.out.println("This is beforeClass of 01");
+	  System.out.println("This is beforeClass of 02");
 
   }
 
   @AfterClass
   public void afterClass() {
-	  System.out.println("This is afterClass of 01");
+	  System.out.println("This is afterClass of 02");
 
   }
 
   @BeforeTest
   public void beforeTest() {
-	  System.out.println("This is beforeTest of 01");
+	  System.out.println("This is beforeTest of 02");
 
   }
 
   @AfterTest
   public void afterTest() {
-	  System.out.println("This is afterTest of 01");
+	  System.out.println("This is afterTest of 02");
 
   }
 
   @BeforeSuite
   public void beforeSuite() {
-	  System.out.println("This is beforeSuite of 01");
+	  System.out.println("This is beforeSuite of 02");
 
   }
 
   @AfterSuite
   public void afterSuite() {
-	  System.out.println("This is afterSuite of 01");
+	  System.out.println("This is afterSuite of 02");
 
   }
 
