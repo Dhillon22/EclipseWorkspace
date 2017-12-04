81d1679890cf9fef3f30f4dbbd6ab444c4a4d868
GS <gaganpreet_dhillon@infosys.com>
2017-12-04 16:05:36 +0530

    New changes

--- FirstTest/src/testPackage/testClass.java
+++ FirstTest/src/testPackage/testClass.java
@@ -9,15 +9,27 @@ import org.openqa.selenium.WebElement;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 
+import java.awt.Frame;
+import java.awt.Point;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import javax.xml.xpath.XPath;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.By.ByClassName;
 import org.openqa.selenium.By.ById;
 import org.openqa.selenium.By.ByName;
 import org.openqa.selenium.By.ByPartialLinkText;
+import org.openqa.selenium.By.ByTagName;
 import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
@@ -28,6 +40,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -38,11 +51,16 @@ import org.testng.annotations.Test;
 public class testClass {
 	WebDriver driver;
 	
+	public testClass(){
+		System.out.println("this is a default constructor");
+	}
+	
+	
 @BeforeTest
 	public void preExecutionCall() {
 	System.setProperty("webdriver.chrome.driver","/Users/infosysuser/Desktop/EclipseWorkspace/chromedriver");
 	 driver = new ChromeDriver();
-	System.out.println("This is the begining of Test");
+	//System.out.println("This is the begining of Test");
 	}
 //@Test
 //	public  void testCaseToOpenGmail() {
@@ -100,13 +118,13 @@ public class testClass {
 ////newActions.moveToElement(target)
 //}
 
-@Test
-public void waitTest(){
-	driver.get("http://www.snapdeal.com/");
-	Wait wait = new FluentWait(driver);
-	wait.until(ExpectedConditions.titleContains("snapdeal"));
-
-}
+//@Test
+//public void waitTest(){
+//	driver.get("http://www.snapdeal.com/");
+//	Wait wait = new FluentWait(driver);
+//	wait.until(ExpectedConditions.titleContains("snapdeal"));
+//
+//}
 @AfterTest
 public void postExecutionCall()
 {
@@ -114,9 +132,53 @@ public void postExecutionCall()
 	System.out.println("This is the End of Test");
 }
 	
+//public void genericWait(String str){
+//	
+//	Wait wait = new FluentWait(driver);
+//	wait.until(ExpectedConditions.titleContains(str));
+//
+//}
 
+//@Test
+//public void readProperties ()  {
+//	File newFile = new File("/Users/infosysuser/Desktop/EclipseWorkspace/FirstTest/src/testPackage/testRead");
+//	FileInputStream inputStream = null;
+//	try {
+//		inputStream = new FileInputStream(newFile);
+//	} catch (FileNotFoundException e1) {
+//		// TODO Auto-generated catch block
+//		e1.printStackTrace();
+//	}
+//	Properties newProperties = new Properties();
+//	try {
+//		newProperties.load(inputStream);
+//	} catch (IOException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//System.out.println(newProperties);
+//	
+//
+//}
 
+//@Test
+//public void minimizeMaximize (){
+//	driver.get("https://www.google.com");
+//	driver.manage().window().maximize();
+//	driver.manage().window().setPosition(new org.openqa.selenium.Point(200, 50));
+//}
 	
+@Test
+public void iFrameTest(){
+	driver.get("http://www.naukri.com");
+	Wait wait = new WebDriverWait(driver, 10);
+	List<WebElement> iframeElements = driver.findElements(By.tagName("iframe"));
+	//wait.until(ExpectedConditions.visibilityOf(iframeElements[0]));
+	Alert alert = driver.switchTo().alert();
+	alert.dismiss();
 	
+	driver.switchTo().frame("iframe#login-iframe.dspN");
+	
+}	
 	
 }
